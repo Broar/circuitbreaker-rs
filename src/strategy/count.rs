3807da2cf936b0526052dc66c902ea42728b27a7
Steven Briggs <gshbriggs@gmail.com>
2016-09-11 17:42:44 -0400

    Implement allow_request()

--- src/strategy/count.rs
+++ src/strategy/count.rs
@@ -1,25 +1,27 @@
 extern crate chrono;
 
 use super::Strategy;
-use self::chrono::DateTime;
+use self::chrono::offset::utc::UTC;
 
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct CountStrategy {
     count: usize,
     threshold: usize,
-    timeout: usize,
+    timeout: i64,
+    start_of_timeout: Option<i64>,
     is_open: bool
 }
 
 impl CountStrategy {
 
     #[allow(dead_code)]
-    pub fn new(threshold: usize, timeout: usize) -> Self {
+    pub fn new(threshold: usize, timeout: i64) -> Self {
         CountStrategy {
             count: 0,
             threshold: threshold,
             timeout: timeout,
+            start_of_timeout: None,
             is_open: false
         }
     }
@@ -27,7 +29,22 @@ impl CountStrategy {
 
 impl Strategy for CountStrategy {
     fn allow_request(&self) -> bool {
-        false
+        if !self.is_open {
+            true
+        }
+
+        else {
+            match self.start_of_timeout {
+                Some(then) => {
+                    let now = UTC::now().timestamp();
+                    now - then >= self.timeout
+                },
+
+                None => {
+                    false
+                }
+            }
+        }
     }
 
     fn success(&mut self) {
@@ -37,6 +54,7 @@ impl Strategy for CountStrategy {
     fn failure(&mut self) {
         self.count += 1;
         if self.count >= self.threshold {
+            self.start_of_timeout = Some(UTC::now().timestamp());
             self.open();
         }
     }
